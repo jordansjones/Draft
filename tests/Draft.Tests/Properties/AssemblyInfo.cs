94947e5e6a2eae92cbea2a0098e4ce787569d7a9
Jordan S. Jones <jordansjones@gmail.com>
2015-04-24 11:19:21 -0600

    Disable test parallelization

--- tests/Draft.Tests/Properties/AssemblyInfo.cs
+++ tests/Draft.Tests/Properties/AssemblyInfo.cs
@@ -1,9 +1,15 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using Xunit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Draft.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("Draft.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+
+[assembly : AssemblyTitle("Draft.Tests")]
+[assembly : AssemblyDescription("")]
+[assembly : AssemblyProduct("Draft.Tests")]
+[assembly : AssemblyCopyright("Copyright ©  2015")]
+[assembly : CollectionBehavior(DisableTestParallelization = true)]
